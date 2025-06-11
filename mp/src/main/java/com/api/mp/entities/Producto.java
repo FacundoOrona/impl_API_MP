0e33bfe00c2e465081b1e129f006f1e9f5678b83
FacundoOrona <fcndorona@gmail.com>
2025-06-11 16:44:04 -0300

    Update Producto.java

--- mp/src/main/java/com/api/mp/entities/Producto.java
+++ mp/src/main/java/com/api/mp/entities/Producto.java
@@ -1,5 +1,17 @@
 package com.api.mp.entities;
 
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
 public class Producto {
-    
+    private int id;
+    private String titulo;
+    private BigDecimal precio;
+
+    public Producto(){
+        this.id = 1;
+        this.titulo = "Camiseta de River";
+        this.precio = new BigDecimal("100.00");
+    }
 }
