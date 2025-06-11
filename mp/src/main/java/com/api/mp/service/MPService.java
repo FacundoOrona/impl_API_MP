a014bea55024809bca4a45c7f5f068df32f673cb
FacundoOrona <fcndorona@gmail.com>
2025-06-11 17:29:40 -0300

    crearPreferenciaParaVendedor

--- mp/src/main/java/com/api/mp/service/MPService.java
+++ mp/src/main/java/com/api/mp/service/MPService.java
@@ -5,6 +5,8 @@ import com.mercadopago.client.preference.PreferenceClient;
 import com.mercadopago.client.preference.PreferenceItemRequest;
 import com.mercadopago.client.preference.PreferenceRequest;
 import com.mercadopago.resources.preference.Preference;
+
+import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -36,7 +38,30 @@ public class MPService {
 
         PreferenceClient preferenceClient = new PreferenceClient();
         Preference preference = preferenceClient.create(preferenceRequest);
-        
+
         return preference.getInitPoint();
     }
+
+    public String crearPreferenciaParaVendedor(Producto producto, String accessTokenVendedor) throws Exception {
+        MercadoPagoConfig.setAccessToken(accessTokenVendedor); // ¡OJO! Token del VENDEDOR
+
+        PreferenceItemRequest item = PreferenceItemRequest.builder()
+                .title(producto.getTitulo())
+                .quantity(1)
+                .currencyId("ARS")
+                .unitPrice(producto.getPrecio())
+                .build();
+
+        PreferenceRequest preferenceRequest = PreferenceRequest.builder()
+                .items(List.of(item))
+                // Esto se usa si querés cobrar comisión como intermediario
+                //.applicationFee(new BigDecimal("10.00")) // por ejemplo 10 ARS
+                .build();
+
+        PreferenceClient preferenceClient = new PreferenceClient();
+        Preference preference = preferenceClient.create(preferenceRequest);
+
+        return preference.getInitPoint();
+    }
+
 }
