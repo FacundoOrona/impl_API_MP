cfbbb315ff35b5941a25150018f339fb9343b53a
FacundoOrona <fcndorona@gmail.com>
2025-06-11 16:44:15 -0300

    Update ProductoRequestDTO.java

--- mp/src/main/java/com/api/mp/entities/ProductoRequestDTO.java
+++ mp/src/main/java/com/api/mp/entities/ProductoRequestDTO.java
@@ -1,5 +1,14 @@
 package com.api.mp.entities;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductoRequestDTO {
-    
+    @NotNull(message = "El ID no puede estar vacio o nulo")
+    int id;
 }
