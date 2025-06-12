e723fe86e558a1050d3a3823dbb5b9aee395ef71
FacundoOrona <fcndorona@gmail.com>
2025-06-12 16:32:06 -0300

    Create Transaccion.java

---
+++ mp/src/main/java/com/api/mp/entities/Transaccion.java
@@ -0,0 +1,41 @@
+package com.api.mp.entities;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String estado;
+
+    private LocalDateTime fecha = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    public Transaccion(String estado, Usuario usuario, Producto producto){
+        this.estado = estado;
+        this.fecha = LocalDateTime.now();
+        this.usuario = usuario;
+        this.producto = producto;
+    }
+}
