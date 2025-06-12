4c7a6ede6c26b6cc8d04423b5642c41201645ef9
FacundoOrona <fcndorona@gmail.com>
2025-06-12 17:11:48 -0300

    Update Producto.java

--- mp/src/main/java/com/api/mp/entities/Producto.java
+++ mp/src/main/java/com/api/mp/entities/Producto.java
@@ -1,17 +1,23 @@
 package com.api.mp.entities;
 
-import java.math.BigDecimal;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Producto {
-    private int id;
-    private String titulo;
-    private BigDecimal precio;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Producto(){
-        this.id = 1;
-        this.titulo = "Camiseta de River";
-        this.precio = new BigDecimal("100.00");
-    }
+    private String nombre;
+
+    private Double precio;
 }
