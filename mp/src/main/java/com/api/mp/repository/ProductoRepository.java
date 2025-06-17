c3e0128e2c14e0a38a6fbf54e7b36eecb18e07a2
FacundoOrona <fcndorona@gmail.com>
2025-06-17 15:16:05 -0300

    Update ProductoRepository.java

--- mp/src/main/java/com/api/mp/repository/ProductoRepository.java
+++ mp/src/main/java/com/api/mp/repository/ProductoRepository.java
@@ -1,8 +1,9 @@
 package com.api.mp.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.api.mp.entities.Producto;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    public List<Producto> findByReservadoTrue();
 }
