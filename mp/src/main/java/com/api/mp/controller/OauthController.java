9b83bd94fba0fe8436561ef31385c57a94b29054
FacundoOrona <fcndorona@gmail.com>
2025-06-17 14:04:48 -0300

    Create OauthController.java

---
+++ mp/src/main/java/com/api/mp/controller/OauthController.java
@@ -0,0 +1,14 @@
+package com.api.mp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.api.mp.service.OauthService;
+
+@RestController
+@RequestMapping("/oauth")
+public class OauthController {
+    
+}
