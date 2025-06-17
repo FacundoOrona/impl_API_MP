7c3db626bf963d6c83f1f9c8ba1170c1b1cdaaca
FacundoOrona <fcndorona@gmail.com>
2025-06-17 15:17:09 -0300

    Update StateOauthRepository.java

--- mp/src/main/java/com/api/mp/repository/StateOauthRepository.java
+++ mp/src/main/java/com/api/mp/repository/StateOauthRepository.java
@@ -1,5 +1,9 @@
 package com.api.mp.repository;
 
-public class StateOauthRepository {
-    
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.api.mp.entities.StateOauth;
+
+public interface StateOauthRepository extends JpaRepository<StateOauth, Long>{
+    Optional<StateOauth> findByState(String state);
 }
