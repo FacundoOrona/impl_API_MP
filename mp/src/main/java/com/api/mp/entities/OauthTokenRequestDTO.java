7682b3b4e89e05f1dafa9a3565afdece66a83501
FacundoOrona <fcndorona@gmail.com>
2025-06-17 15:14:53 -0300

    Update OauthTokenRequestDTO.java

--- mp/src/main/java/com/api/mp/entities/OauthTokenRequestDTO.java
+++ mp/src/main/java/com/api/mp/entities/OauthTokenRequestDTO.java
@@ -1,5 +1,32 @@
 package com.api.mp.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OauthTokenRequestDTO {
-    
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("public_key")
+    private String publicKey;
+
+    @JsonProperty("user_id")
+    private Long userId;
+
+    @JsonProperty("expires_in")
+    private Long expiresIn; //Segundos
+
+    @JsonProperty("live_mode")
+    private boolean liveMode;
+
 }
