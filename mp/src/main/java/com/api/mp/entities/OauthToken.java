38fcffd0914e090ce0c765ed9176177a5fde1a32
FacundoOrona <fcndorona@gmail.com>
2025-06-17 15:14:43 -0300

    implementacion oauthToken

--- mp/src/main/java/com/api/mp/entities/OauthToken.java
+++ mp/src/main/java/com/api/mp/entities/OauthToken.java
@@ -1,5 +1,42 @@
 package com.api.mp.entities;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "oauth_token")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OauthToken {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String accessToken;
+    private String refreshToken;
+    private String publicKey;
+    private Long userId;
+    private LocalDateTime expiresAt;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    private boolean liveMode;
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expiresAt);
+    }
+
 }
