ca9e6961a6cbdd389933d21fb99cfe4601a16b3e
FacundoOrona <fcndorona@gmail.com>
2025-06-17 15:15:54 -0300

    Update OauthTokenRepository.java

--- mp/src/main/java/com/api/mp/repository/OauthTokenRepository.java
+++ mp/src/main/java/com/api/mp/repository/OauthTokenRepository.java
@@ -1,5 +1,8 @@
 package com.api.mp.repository;
 
-public class OauthTokenRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.api.mp.entities.OauthToken;
+
+public interface OauthTokenRepository extends JpaRepository<OauthToken, Long>{
     
 }
