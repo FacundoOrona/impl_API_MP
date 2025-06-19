bfaf438001e7232ec7b42c59f089ea79f04103d0
FacundoOrona <fcndorona@gmail.com>
2025-06-19 17:09:39 -0300

    AccessTokenValido OauthService

--- mp/src/main/java/com/api/mp/service/OauthService.java
+++ mp/src/main/java/com/api/mp/service/OauthService.java
@@ -5,11 +5,14 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.api.mp.entities.*;
@@ -91,6 +94,26 @@ public class OauthService {
         return oauthRepository.findByUsuarioId(id)
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado por ID"))
                 .getAccessToken();
+    }
+
+    public boolean AccessTokenValido(String accessToken) {
+        String url = "https://api.mercadopago.com/users/me";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            return response.getStatusCode().is2xxSuccessful();
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED || e.getStatusCode() == HttpStatus.FORBIDDEN) {
+                return false;
+            }
+            return false;
+        }
     }
 
 }
