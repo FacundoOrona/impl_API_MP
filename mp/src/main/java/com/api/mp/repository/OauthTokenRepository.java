0757a9fa16fa9a2d5cf6bb2145f307a285c41c29
FacundoOrona <fcndorona@gmail.com>
2025-06-19 17:00:19 -0300

    findByUsuarioId OauthTokenRepository

--- mp/src/main/java/com/api/mp/repository/OauthTokenRepository.java
+++ mp/src/main/java/com/api/mp/repository/OauthTokenRepository.java
@@ -1,8 +1,9 @@
 package com.api.mp.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.api.mp.entities.OauthToken;
 
 public interface OauthTokenRepository extends JpaRepository<OauthToken, Long>{
-    
+    Optional<OauthToken> findByUsuarioId(Long userId);
 }
