669b79cf3ce7a91447f2bda06170db6b25d40ca9
FacundoOrona <fcndorona@gmail.com>
2025-06-24 16:27:42 -0300

    refrescarTokens & tokenExpirado

--- mp/src/main/java/com/api/mp/scheduled/TokenRefresherService.java
+++ mp/src/main/java/com/api/mp/scheduled/TokenRefresherService.java
@@ -8,15 +8,40 @@ import com.api.mp.entities.OauthToken;
 import com.api.mp.entities.OauthTokenRequestDTO;
 import com.api.mp.service.MPService;
 import com.api.mp.service.OauthService;
-//import com.api.mp.util.EncriptadoUtil;
+import com.api.mp.util.EncriptadoUtil;
 
 @Service
 public class TokenRefresherService {
     
     private final MPService mercadoPagoService;
     private final OauthService oauthService;
-    //private final EncriptadoUtil encriptadoUtil;
+    private final EncriptadoUtil encriptadoUtil;
 
+    public TokenRefresherService(MPService mercadoPagoService, OauthService oauthService, EncriptadoUtil encriptadoUtil){
+        this.mercadoPagoService = mercadoPagoService;
+        this.oauthService = oauthService;
+        this.encriptadoUtil = encriptadoUtil;
+    }
+
+    @Scheduled(fixedRate = 3600000) // cada 1 hora (en milisegundos)
+    public void refrescarTokens() {
+        List<OauthToken> tokens = oauthService.obtenerTokenDeUsuariosVendedores();
+        for (OauthToken token : tokens) {
+            if (tokenExpirado(token)) {
+                try {
+                    OauthTokenRequestDTO nuevoToken = mercadoPagoService.refrescarToken(encriptadoUtil.desencriptar(token.getRefreshToken()));
+                    oauthService.actualizarToken(nuevoToken, token.getUsuario());
+                } catch (Exception e) {
+                    System.err.println("Error actualizando token para usuario " + token.getUsuario().getNombre());
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    private boolean tokenExpirado(OauthToken token) {
+        return token.getExpiresAt().isBefore(LocalDateTime.now().plusMinutes(11)); // margen de seguridad
+    }
 
 }
 
