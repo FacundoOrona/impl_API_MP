2952020adcaec6699c6f5e0664eaa2c2c4bb7115
FacundoOrona <fcndorona@gmail.com>
2025-06-24 17:20:39 -0300

    Update OauthController.java

--- mp/src/main/java/com/api/mp/controller/OauthController.java
+++ mp/src/main/java/com/api/mp/controller/OauthController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 import com.api.mp.service.OauthService;
 
 @RestController
@@ -18,14 +19,26 @@ public class OauthController {
     }
 
     @GetMapping("/init")
-    public String init() {
-        return oauthService.UrlAutorizacion();
+    public ResponseEntity<String> init() {
+        try{
+            return ResponseEntity.ok(oauthService.UrlAutorizacion());
+        }
+        catch(Exception e){
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("Error al crear URL de autenticación: " + e.getMessage());
+        }
     }
 
     @GetMapping("/callback")
-    public ResponseEntity<String> callback(@RequestParam("code") String code,
-            @RequestParam("state") String state) {
-        String resultado = oauthService.obtenerAccesToken(code, state);
-        return ResponseEntity.ok(resultado);
+    public ResponseEntity<String> callback(@RequestParam("code") String code, @RequestParam("state") String state) {
+        try {
+            oauthService.obtenerAccessToken(code, state);
+            return ResponseEntity.ok("Autenticación con Mercado Pago completada correctamente.");
+        } catch (RuntimeException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Error al completar la autenticación: " + e.getMessage());
+        }
     }
 }
